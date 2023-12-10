ec75e37f8f81f1bc0f186fe113d7834f6cfdd2b9
Aranza Quijano <91640852+araloul@users.noreply.github.com>
2023-12-09 21:06:38 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+let valorAnterior = document.getElementsByClassName("valorAnterior");
+let valorActual = document.getElementsByClassName("valorActual");
+let btnEspecial = document.querySelectorAll("btnEspecial");
+let btn = document.querySelectorAll("btn");
+let calculadora = document.getElementsByClassName("calculadora")
+let limpiar = document.getElementById("limpiar");
 
+limpiar.addEventListener("click", function (event){
+    event.preventDefault();
+    console.log(click);
+
+});
+
+
+btnEspecial.addEventListener("click", function (event){
+    event.preventDefault();
+    console.log(click);
+
+});
